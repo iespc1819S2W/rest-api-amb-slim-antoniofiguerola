d092f6fd7de642fcdcc7a69695bb33cac9994b62
unknown <antoniofiguerola@iespaucasesnoves.cat>
2019-01-11 23:50:56 +0100

    canvis 11 de gener

---
+++ app/route/llibre_route.php
@@ -0,0 +1,69 @@
+<?php
+use App\Model\Llibre;
+
+$app->group('/llibre/', function () {
+
+    $this->get('', function ($req, $res, $args) {
+        $obj = new Llibre();   
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $obj->mostrarTodos()
+            )
+        );
+    });
+    
+    $this->get('{id}', function ($req, $res, $args) {
+        $obj = new Llibre();   
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $obj->llegirLlibrePK($args["id"])
+            )
+        );         
+    });
+           
+    $this->post('', function ($req, $res, $args) {
+            $atributs=$req->getParsedBody();  //llista atributs del client
+            $obj = new Llibre();
+            return $res
+               ->withHeader('Content-type', 'application/json')
+               ->getBody()
+               ->write(
+                json_encode(
+                    $obj->altaLlibre($atributs)
+                )
+            );             
+    });
+
+    $this->put('{id}', function ($req, $res, $args) {
+        $atributs=$req->getParsedBody();  //llista atributs del client
+        $atributs["id"]=$args["id"];     // Afegim id a la llista d'atributs
+        $obj = new Llibre();
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $obj->update($atributs)
+            )
+        ); 
+    });  
+    
+    $this->delete('{id}', function ($req, $res, $args) {
+        $obj = new Llibre();   
+        return $res
+           ->withHeader('Content-type', 'application/json')
+           ->getBody()
+           ->write(
+            json_encode(
+                $obj->delete($args["id"])
+            )
+        ); 
+    });
+        
+});
